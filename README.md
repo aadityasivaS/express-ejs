97e5141d0b05b26289ec281448920ecd97a2ee79
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2020-11-17 14:07:03 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Express EJS template (Note this is just the template the npm package will be comming soon)
+
+a npm package which generates starter code for [Express](http://expressjs.com/) and [EJS](https://ejs.co/)
+
+## Getting Started
+
+To get started follow the instruction below
+
+### Installing
+
+To install run
+
+```
+npm i -g express-ejs
+```
+
+### Usage
+
+To start run
+```
+express-ejs your-project-name
+```
+
+This will clone all the files and install all packages
+
+To start the server run
+
+```
+npm start
+```
+
+This will start the server at port 3000
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+
+## Contributing
+
+Feel free to submit pull requests
+
+## Author
+
+* **Aadityasiva** - *All work* - [aadityasivaS](https://github.com/aadityasivaS)
+
---
+++ index.js
@@ -0,0 +1,46 @@
+var { exec } = require('child_process');
+var fs = require('fs');
+var npm = require('npm-programmatic');
+const simpleGit = require('simple-git');
+require('colors');
+const git = simpleGit();
+var Args = process.argv.slice(1);
+console.log('Making your project');
+const loading = require('loading-cli');
+const load = loading("Cloning Template!!").start()
+let packageMod = `{
+    "name": "${Args[0]}",
+    "version": "1.0.0",
+    "description": "",
+    "main": "index.js",
+    "scripts": {
+      "start": "nodemon index.js"
+    },
+    "license": "ISC",
+    "dependencies": {
+      "ejs": "^3.1.5",
+      "express": "^4.17.1"
+    }
+  }`;
+exec(`git clone https://github.com/aadityasivaS/express-ejs-template.git ${Args[0]}`, (err, stdout, stderr) => {
+  load.color = 'yellow';
+  load.text = ' Installing Packages';
+  if (!err) {
+    fs.writeFile(`${Args[0]}/package.json`, packageMod, function (err) {
+      npm.install(['express', 'ejs', 'nodemon'], {
+        cwd: `${Args[0]}`,
+        save: true,
+      })
+        .then(function () {
+          git.cwd(`${Args[0]}`);
+          git.add(['package.json', 'package-lock.json']).then(function () {
+            git.commit('Cloned repo', (err) => {
+              load.stop();
+              console.log("SUCCESS!!! run npm start in the directory to start the server".green);
+            });
+          });
+
+        })
+    });
+  }
+});
