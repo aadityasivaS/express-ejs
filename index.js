936e1b953e2a1ffbb8658a1babf8c9fea2abb048
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2021-01-02 07:28:26 +0530

    New version

--- index.js
+++ index.js
@@ -3,20 +3,7 @@ var { exec } = require('child_process');
 var fs = require('fs');
 var npm = require('npm-programmatic');
 const simpleGit = require('simple-git');
-const firebase = require('firebase/app');
-require('firebase/analytics');
 require('colors');
-var firebaseConfig = {
-  apiKey: "AIzaSyDWVPl0eBGOvVkGce-h-0IDtkc--cwWtUc",
-  authDomain: "express-ejs.firebaseapp.com",
-  projectId: "express-ejs",
-  storageBucket: "express-ejs.appspot.com",
-  messagingSenderId: "373283735977",
-  appId: "1:373283735977:web:7c1dc597146fbce5ecdcb8",
-  measurementId: "G-W2TFPD40CF"
-};
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 const git = simpleGit();
 var Args = process.argv.slice(2);
 console.log('Making your project');
